a607f38865f2c8d7b640f786f2b1b06d97ed3aaa
QYZ <1904375104@qq.com>
2022-07-14 18:40:02 +0800

    get ip

---
+++ src/main/java/com/example/demo/controller/IpController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping
+public class IpController {
+    @RequestMapping("/ip")
+    public String getIP(HttpServletRequest request){
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        System.out.println(ip);
+        return ip;
+    }
+}
